41e861276c653a47750025e3a5f5eaf97a80d921
sajitha <ThnG892Rt>
2023-10-24 13:24:04 +1000

    state update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
+
+  const [step, setStep] = useState(1);
+  let [test, updateName] = useState({ name: 'sajitha' })
+
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+    // test.name = 'sajj';
+    updateName({ name: 'hemm' })
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+
+    // bad pratice , always use use update
+    // test.name = 'hem';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]} {test.name}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>Prevous</button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>Next</button>
+      </div>
+    </div >
   );
 }
 
